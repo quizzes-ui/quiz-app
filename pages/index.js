5ffcf4c034223a0e4fd8f884f3e22dc976da3431
Raul <raulbravol@gmail.com>
2024-11-16 17:37:39 +0100

    with database connection

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import Quiz from '../components/Quiz';
-import DatabaseDisplay from '../components/DatabaseDisplay';
+import TestDB from './components/TestDB';
 
 const Home = () => {
   return (
     <div>
-      <DatabaseDisplay />
+      <TestDB />
       <Quiz />
     </div>
   );
 };
 
 export default Home;
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Quiz from './components/Quiz';
+import TestDB from './components/TestDB';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <TestDB />
+    <Quiz />
+  </React.StrictMode>
