5642155fbe38cef183a4ff39d7367d80bf3bb7de
Raul <raulbravol@gmail.com>
2024-11-16 17:41:32 +0100

    Version DB

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Quiz from '../components/Quiz';
-import TestDB from '../components/TestDB';
+import TestDB from '../components/testDB';
 
 const Home = () => {
   return (
