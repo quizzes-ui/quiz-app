3ad480eff4d15fa4be6af78402334d59279853db
Raul <raulbravol@gmail.com>
2024-11-16 17:39:10 +0100

    With DB ok

--- pages/index.js
+++ pages/index.js
@@ -13,17 +13,3 @@ const Home = () => {
 
 export default Home;
 
-
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Quiz from './components/Quiz';
-import TestDB from './components/TestDB';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <TestDB />
-    <Quiz />
-  </React.StrictMode>
