131b8d7c98c81b7a14e918fa9e7a04d87e146867
Raul <raulbravol@gmail.com>
2024-11-17 11:21:29 +0100

    new db

--- components/questionsDB.js
+++ components/questionsDB.js
@@ -17,7 +17,7 @@ export default function QuestionsDB() {
     async function fetchQuestions() {
       try {
         const { data, error } = await supabase
-          .from('quizzes')
+          .from('question-files')
           .select('id, question_file_name')
 
         if (error) throw error
