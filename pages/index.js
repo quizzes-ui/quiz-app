321bc7ab45e3559da9ee4ffd2bdd9d9baa8d767a
Raul <raulbravol@gmail.com>
2024-11-20 19:46:57 +0100

    test db

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Quiz from '../components/Quiz';
 const Home = () => {
   return (
     <div>
+      <TestDB />
       <Quiz />
       <div className="version-tag">Version 3.1</div>
     </div>
