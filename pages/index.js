510546a9bee136f5886890ead0e95b87c2b204ff
Raul <raulbravol@gmail.com>
2024-11-23 14:26:12 +0100

    TestDB ok

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Quiz from '../components/Quiz';
-import Quiz from '../components/TestDB';
+import TestDB from '../components/TestDB';
 
 const Home = () => {
   return (
