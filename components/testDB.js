05db16becbcd1f5b7f11eef5b988853d137b6605
Raul <raulbravol@gmail.com>
2024-11-23 14:35:20 +0100

    db

--- components/testDB.js
+++ components/testDB.js
@@ -1,6 +1,5 @@
 'use client'
-
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js'
 
 const TestDB = () => {
