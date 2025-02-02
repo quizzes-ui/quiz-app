ad391670508023c50b76bf4a72fe7c8653a05ec9
Raul <raulbravol@gmail.com>
2025-02-02 17:06:04 +0100

    test-blob

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Quiz from '../components/Quiz';
+import { put } from "@vercel/blob";
+
+
 
 
 const Home = () => {
   return (
     <div>
-      
+      const { url } = await put('articles/blob.txt', 'Hello World!', { access: 'public' });
       <Quiz />
       <div className="version-tag">Version 3.5</div>
     </div>
