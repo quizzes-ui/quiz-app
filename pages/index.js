6e84d3dd4731aff9c6333e2a493864bfd7e1db88
Raul <raulbravol@gmail.com>
2025-02-02 17:25:26 +0100

    now

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,7 @@ const Home = () => {
   return (
     <div>
       <Quiz />
+      
       <div className="version-tag">Version 3.5</div>
       {url && <p>File uploaded: <a href={url} target="_blank" rel="noopener noreferrer">{url}</a></p>}
     </div>
